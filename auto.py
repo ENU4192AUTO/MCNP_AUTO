a0aa2293f6e0f6d2421ed70ba8b72ea257bac1d1
ENU4192AUTO <mcnpautomata@gmail.com>
2020-01-19 14:38:33 -0500

    Updated Auto

--- auto.py
+++ auto.py
@@ -1,17 +1,22 @@
 try:
     from Ascension import runner
     from glob import glob
-    from os import rename, remove
-    from os import chdir, system
+    from os import rename, remove,chdir, system
+
     import numpy as np
 except:
     system("py -m pip install -r requirements.txt")
     from Ascension import runner
     from glob import glob
-    from os import rename
-    from os import chdir, system
+    from os import  rename, remove,chdir, system
     import numpy as np
+G=open("LOGO.txt")
+logo = G.read()
+G.close()
 chdir("..")
+logo=eval(f'f"""{logo}"""')
+print(f"{logo}The program will run {len(glob('*.i'))} .i files")
+
 try:
     F=open("keff.csv",'r')
     F.close()
@@ -23,12 +28,14 @@ for name in glob("*.i"):
     print(name)
     system(f'title Now Running:  {name}')
     toaddr,keff=runner(name)
-    it=0
+
     for item in keff:
         F=open("keff.csv",'a')
         F.write(f"{name[:name.find('.')]},{name[name.find('.')+1:name.find('_')]},{item[0]},{item[1]}\n")
         F.close()
         it+=1
+    # remove(name[:len(name)-1]+".r")
+    # remove(name[:len(name)-1]+".s")
     system(f'title: Automata')
     rename(name,name+"np")
                                
